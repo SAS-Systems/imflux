4ded9b802f7114b3c20c1e9bb32816b6b7bfaa9c
Schmidl <D062321@global.corp.sap>
2016-05-30 17:35:56 +0200

    test jira ticketing

--- send_jira_ticket.sh
+++ send_jira_ticket.sh
@@ -2,4 +2,4 @@
 curl -i --user $JIRA_USER:$JIRA_PASS \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
--X POST -d '{"fields": {"project":{ "key": "UNV"}, "summary": "Travis Build Error: '"$TRAVIS_BRANCH"'", "description": "The build process of commit: '"$TRAVIS_COMMIT"' was not successful. Please visit https://travis-ci.org/SAS-Systems/imflux'"$TRAVIS_BUILD_ID"' This information was automatically created. Please add further instructions.",  "issuetype": {"name": "Bug" } } }' jira.it.dh-karlsruhe.de:8080/rest/api/2/issue/
+-X POST -d '{"fields": {"project":{ "key": "UNV"}, "summary": "Travis Build Error: '"$TRAVIS_BRANCH"'", "description": "The build process of commit: '"$TRAVIS_COMMIT"' was not successful. Please visit https://travis-ci.org/SAS-Systems/imflux/builds/'"$TRAVIS_BUILD_ID"' This information was automatically created. Please add further instructions.",  "issuetype": {"name": "Bug" } } }' jira.it.dh-karlsruhe.de:8080/rest/api/2/issue/
